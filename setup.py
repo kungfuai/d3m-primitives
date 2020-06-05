985acacd7e7eaf592f4ed5de480a37d42706a084
jlgleason <jeffrey.gleason.3@gmail.com>
2020-06-05 16:35:24 -0500

    bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kf-d3m-primitives",
-    version="0.1.0",
+    version="0.2.0",
     description="All Kung Fu D3M primitives as a single library",
     packages=find_packages(),
     setkeywords=['d3m_primitive'],
