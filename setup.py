6d152f2bc2856a9834e7729acc9e400d4b58579d
jlgleason <jeffrey.gleason.3@gmail.com>
2020-06-24 13:40:47 -0500

    bump simon TF dep and package version (0.4.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kf-d3m-primitives",
-    version="0.3.0",
+    version="0.4.0",
     description="All Kung Fu D3M primitives as a single library",
     packages=find_packages(),
     setkeywords=['d3m_primitive'],
@@ -21,7 +21,7 @@ setup(
         "scikit-image==0.16.1",
         "punk @ git+https://github.com/uncharted-distil/punk@8b101eca26b5f9a3df2a65aab2733bd404965578#egg=punk",
         "object_detection_retinanet @ git+https://github.com/uncharted-distil/object-detection-retinanet@f02652a00b3cd81a5c37f5c34da0daaa541dffb1#egg=object_detection_retinanet",
-        "Simon @ git+https://github.com/uncharted-distil/simon@9059b633765c3b5adabbfa9ac55591db3e665a2d#egg=Simon-1.2.4",
+        "Simon @ git+https://github.com/uncharted-distil/simon@00422bbdc9caa09b867f8b5f583487b59b605de0#egg=Simon-1.2.5",
         "nk_sent2vec @ git+https://github.com/uncharted-distil/nk-sent2vec@08a74ce1aff98e81eda2f3211ad7f7015bfa8124#egg=nk_sent2vec",
         "duke @ git+https://github.com/uncharted-distil/duke@c56416e959b52ff5077c5a54c329e2f6e83bbd97#egg=duke",
         "rsp @ git+https://github.com/cfld/rs_pretrained@92d832efe1961d6a06011f689dad7ef2481a64b1#egg=rsp"
